416b3a91b50e7120222b1309a310a51d5a695688
Gonzalo Pelenur <gon@dhcp-10-250-243-40.harvard.edu>
2024-02-06 20:15:41 -0500

    added back text

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold text-align-left">Gonzalo Pelenur</h1>
-      <p>Hello</p>
-      <p>Hello</p>
+      <p>Hello! My name is Gonzalo Pelenur, and I am a Harvard Computer Science student. I have 6+ years of programming experience, mainly programming on Python, JavaScript, and C++. I'm currently working at the Derek Bok Center for Teaching and Learning on Harvard University as a Generative AI Course Asistant (GAICA). I enjoy working on all sorts of software engineering projects, that could be around systems programming, machine learning, networks, full stack development, data science, or more.</p>
+      <p>Some relevant courses that I took at Harvard or I am currently taking are: CS61 Systems Programming, CS145 Networking At Scale, CS120 Algorithms & Limitations, STAT110 Statistics, CS124 Data Structures and Algorithms, AM120 Applied Linear Algebra and Big Data.</p>
     </main>
   );
 }
