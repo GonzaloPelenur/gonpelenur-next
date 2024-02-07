bfe80c96e31072135669b094c28187d862e09a00
Gonzalo Pelenur <gon@dhcp-10-250-243-40.harvard.edu>
2024-02-06 19:33:33 -0500

    sample text

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
       <link rel='icon' href='/favicon.ico' />
-
       </head>
-
       <body className={inter.className}>{children}</body>
     </html>
   );
